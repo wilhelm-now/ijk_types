c97a6843158586296688cdfda6e52d97471d92c4
WILHELM <wilhelm.now@gmail.com>
2024-11-17 17:35:22 +0100

    Name direction vectors, even W.
    TBD: Should W be named or just be a number?

--- include/quat.h
+++ include/quat.h
@@ -1,1 +1,32 @@
 #pragma once
+
+#include <concepts>
+#include <compare>
+
+template<std::floating_point T, typename direction>
+class directed_value
+{
+	T val;
+public:
+	constexpr explicit directed_value(T v)
+		: val(v)
+	{}
+
+	auto operator<=>(directed_value const&) const = default;
+
+	T value() const
+	{
+		return val;
+	}
+};
+
+#define NAME_DIRECTION(dir) \
+struct dir##_tag{}; \
+template<typename T> \
+using dir = directed_value<T, dir##_tag>; \
+
+
+NAME_DIRECTION(I)
+NAME_DIRECTION(J)
+NAME_DIRECTION(K)
+NAME_DIRECTION(W)
--- tests/basic.cpp
+++ tests/basic.cpp
@@ -2,4 +2,11 @@
 
 static_assert(true, "can compile");
 
+static constexpr auto plus_i = I{ 1. };
+static constexpr auto plus_j = J{ 1. };
+static constexpr auto plus_k = K{ 1. };
+
+static_assert(plus_i == I{ 1. });
+static_assert(plus_i != I{ 123.456 });
+
 int main(){}
