ea43289471d224fab8e020b162bab1836297f0d7
WILHELM <wilhelm.now@gmail.com>
2024-11-17 19:20:33 +0100

    Implement direction multiplication identities ii = jj = kk = ijk = -1

--- include/quat.h
+++ include/quat.h
@@ -3,6 +3,7 @@
 #include <concepts>
 #include <compare>
 
+
 template<std::floating_point T, typename direction>
 class directed_value
 {
@@ -14,19 +15,68 @@ public:
 
 	auto operator<=>(directed_value const&) const = default;
 
-	T value() const
+	constexpr T value() const
 	{
 		return val;
 	}
 };
 
-#define NAME_DIRECTION(dir) \
-struct dir##_tag{}; \
-template<typename T> \
-using dir = directed_value<T, dir##_tag>; \
+template<typename T, typename U, typename direction>
+using common_dir = directed_value<std::common_type_t<T, U>, direction>;
 
+template<std::floating_point T, std::floating_point U, typename direction>
+constexpr common_dir<T, U, direction> operator+(directed_value<T, direction> const& LHS, directed_value<U, direction> const& RHS)
+{
+	return common_dir<T, U, direction>(LHS.value() + RHS.value());
+}
+
+template<std::floating_point T, typename direction>
+constexpr directed_value<T, direction> operator-(directed_value<T, direction> const& RHS)
+{
+	return directed_value<T, direction>(-RHS.value());
+}
+
+template<std::floating_point T, std::floating_point U, typename direction>
+constexpr common_dir<T, U, direction> operator-(directed_value<T, direction> const& LHS, directed_value<U, direction> const& RHS)
+{
+	return common_dir<T, U, direction>(LHS.value() - RHS.value());
+}
+
+
+#define NAME_DIRECTION(dir) \
+struct dir##_dir{}; \
+template<typename T> \
+using dir = directed_value<T, dir##_dir>; \
 
 NAME_DIRECTION(I)
 NAME_DIRECTION(J)
 NAME_DIRECTION(K)
-NAME_DIRECTION(W)
+NAME_DIRECTION(W)  // TODO: decide if this needs a direction or should be a plain number
+
+// Now applying quaternion identities
+// ii == jj == kk == ijk == -1 (w in this case)
+// These lead to
+// i = jk = -kj
+// j = -ik = ki
+// k = ij = -ji 
+
+// ii, jj, kk covered by requiring same direction tag for 
+template<typename T, typename U, typename direction>
+constexpr directed_value<std::common_type_t<T, U>, W_dir> operator*(directed_value<T, direction> const& LHS, directed_value<U, direction> const& RHS)
+{
+	return directed_value<std::common_type_t<T, U>, W_dir>(-LHS.value() * RHS.value());
+}
+
+#define MULTIPLY_DIRS(LHS_DIR, RHS_DIR, RESULT_DIR, RESULT_SIGN) \
+template<typename T, typename U> \
+constexpr directed_value<std::common_type_t<T, U>, RESULT_DIR> operator*(directed_value<T, LHS_DIR> const& LHS, directed_value<U, RHS_DIR> const& RHS) \
+{ \
+	return directed_value<std::common_type_t<T, U>, RESULT_DIR>(RESULT_SIGN LHS.value() * RHS.value()); \
+}
+
+MULTIPLY_DIRS(J_dir, K_dir, I_dir, +) // jk = i
+MULTIPLY_DIRS(K_dir, J_dir, I_dir, -) // kj = -i
+MULTIPLY_DIRS(I_dir, K_dir, J_dir, -) // ik = -j
+MULTIPLY_DIRS(K_dir, I_dir, J_dir, +) // ki = j
+MULTIPLY_DIRS(I_dir, J_dir, K_dir, +) // ij = k
+MULTIPLY_DIRS(J_dir, I_dir, K_dir, -) // ji = -k
--- tests/basic.cpp
+++ tests/basic.cpp
@@ -9,4 +9,21 @@ static constexpr auto plus_k = K{ 1. };
 static_assert(plus_i == I{ 1. });
 static_assert(plus_i != I{ 123.456 });
 
+static_assert(plus_j == -J{ -1. }, "negation");
+
+// Per component addition and subtraction
+static_assert(plus_k + plus_k == K{ 2. }, "addition");
+static_assert(K{ 3. } - plus_k == K{ 2. }, "subtraction");
+
+// direction identities
+static_assert(plus_j * plus_k == plus_i, "jk = i");
+static_assert(plus_k * plus_j == -plus_i, "kj = -i");
+static_assert(plus_i * plus_k == -plus_j, "ik = -j");
+static_assert(plus_k * plus_i == plus_j, "ki = j");
+static_assert(plus_i * plus_j == plus_k, "ij = k");
+static_assert(plus_j * plus_i == -plus_k, "ji = -k");
+
+static_assert(plus_i * plus_j * plus_k == W{ -1. }, "still not sure if result should be a directed value");
+
+// If it compiles it's ok
 int main(){}
